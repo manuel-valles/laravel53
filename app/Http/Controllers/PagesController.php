1b3adf8145a1170b54910ce2cb0747d54f6d4f5c
manukempo <manukempo@gmail.com>
2017-08-11 16:13:28 +0100

    Mutators, Assessors, Views

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use View;
+
+class PagesController extends Controller
+{
+    public function index()
+    {
+    	if(View::exists('pages.index'))
+    		// return view('pages.index', ['text' => '<b>Laravel</b>']);
+    		return view('pages.index')
+    			->with('text','<b>Laravel</b>')
+    			->with('name', 'Nicole')
+    			->with(['location'=>'Europe', 'planet'=>'Earth']);
+    	else
+ 		   	return 'No view avaliable';
+    }
+}
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+// Import View
+use View;
+// Import Carbon for dates
+use Carbon\Carbon;
+// Import Auth
+use Auth;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // share('key', 'value')
+        View::share('myname', 'manu');
+        // Access the attribute age from the function createFromDate using carbon
+        $age = Carbon::createFromDate(1993, 7, 6)->age;
+        View::share('age', $age);
+        //If you want to assess the auth data, you need the composer. Where * means the data will be available to all views;
+        View::composer('*', function($view){
+                $view->with('auth', Auth::user());
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
