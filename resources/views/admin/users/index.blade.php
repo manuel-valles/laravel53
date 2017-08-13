d5865f9ee54827a04253ab0f0d5e9f1b4d945b40
manukempo <manukempo@gmail.com>
2017-08-13 12:24:10 +0100

    Middleware and Route Group

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -5,9 +5,8 @@
 	<div class="col-md-6 col-md-offset-3">
 		<h3>{{$users->total()}} total users</h3>
 		<b>In this page ({{$users->count()}} users)</b>
-
 		<ul class="list-group">
-			@foreach($users as $user)
+			@forelse($users as $user)
 				<li class="list-group-item" style="margin-top:20px">
 					<span>
 						{{$user->name}}
@@ -17,7 +16,9 @@
 						<button class="btn btn-xs btn-primary">Follow</button>
 					</span>
 				</li>
-			@endforeach
+			@empty
+				<p>No users available</p>
+			@endforelse
 			{{$users->links()}}
 		</ul>
 	</div>
--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,18 @@
 //     return view('welcome');
 // });
 Route::get('/', 'PagesController@index');
-Route::get('profile','PagesController@profile');
-Route::get('settings','PagesController@settings');
+
 Route::get('blade','PagesController@blade');
 Route::get('users',['uses' => 'UsersController@index']);
+
 
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware'=>['authenticated', 'anothermiddleware']], function(){
+	Route::get('profile','PagesController@profile');
+	Route::get('settings','PagesController@settings');
+	Route::get('users','UsersController@index');
+});
